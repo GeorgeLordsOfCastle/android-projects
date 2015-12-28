092540df852fe922d3126e0869f7a921abeefb84
GeorgeLordsOfCastle <juri.castellani@gmail.com>
2015-12-28 20:13:36 +0100

    autodocumentate layout file name done.

--- tutorials/HelloWorld/src/com/jc/android/tutorial/HelloWorld/HelloWorldActivity.java
+++ tutorials/HelloWorld/src/com/jc/android/tutorial/HelloWorld/HelloWorldActivity.java
@@ -10,6 +10,6 @@ public class HelloWorldActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.hello_world_activity);
     }
 }
