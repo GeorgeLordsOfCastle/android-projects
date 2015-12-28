62a7e55e712cf89906529cd22ff81e93c1103baa
GeorgeLordsOfCastle <juri.castellani@gmail.com>
2015-12-28 20:52:17 +0100

    Settings menu added.

--- tutorials/HelloWorld/src/com/jc/android/tutorial/HelloWorld/HelloWorldActivity.java
+++ tutorials/HelloWorld/src/com/jc/android/tutorial/HelloWorld/HelloWorldActivity.java
@@ -2,6 +2,7 @@ package com.jc.android.tutorial.HelloWorld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class HelloWorldActivity extends Activity {
     /**
@@ -11,5 +12,12 @@ public class HelloWorldActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.hello_world_menu, menu);
+        return true;
     }
 }
