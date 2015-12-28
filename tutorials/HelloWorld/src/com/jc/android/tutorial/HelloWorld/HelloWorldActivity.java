f835c8cc90829302284bf6848a6391ec3025c2db
GeorgeLordsOfCastle <juri.castellani@gmail.com>
2015-12-28 20:58:22 +0100

    Settings menu added 3.

--- tutorials/HelloWorld/src/com/jc/android/tutorial/HelloWorld/HelloWorldActivity.java
+++ tutorials/HelloWorld/src/com/jc/android/tutorial/HelloWorld/HelloWorldActivity.java
@@ -3,6 +3,7 @@ package com.jc.android.tutorial.HelloWorld;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class HelloWorldActivity extends Activity {
     /**
@@ -19,5 +20,17 @@ public class HelloWorldActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.hello_world_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
