825d39023403544d5278b13499ef43d4891e8615
GeorgeLordsOfCastle <juri.castellani@gmail.com>
2016-01-02 00:18:27 +0100

    Event handling example void files added 2.

---
+++ tutorials/EventHandlingExample/src/com/jc/android/tutorial/eventHandlingExample/EventHandlingExampleActivity.java
@@ -0,0 +1,15 @@
+package com.jc.android.tutorial.eventHandlingExample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventHandlingExampleActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_handling_example_activity);
+    }
+}
